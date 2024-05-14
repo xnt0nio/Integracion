2f508ac3b796f8abeb882caab4337a34c3b6f7a0
Antonio <an.martinezc@duocuc.cl>
2024-05-14 00:27:57 -0400

    casi listo mercado pago

--- core/static/core/js/app.js
+++ core/static/core/js/app.js
@@ -2,39 +2,58 @@
 
 
 
-const mp = new MercadoPago('TEST-99b079b4-d97b-4ea4-bf60-23112846af0a', {
-    locale: "es-CH",
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const mp = new MercadoPago('TEST-99b079b4-d97b-4ea4-bf60-23112846af0a', {
+        locale: "es-CH",
+    });
 
-document.getElementById("checkout-btn").addEventListener("click",async () =>{
-    try{
+    document.getElementById("checkoutBtn").addEventListener("click", async () => {
+        try {
+            const orderData = {
+                title: "producto",
+                quantity: 1,
+                price: 10,
+            };
+
+            const response = await fetch("http://localhost:3000/create_preference", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(orderData),
+            });
+
+            const preference = await response.json();
+            console.log(preference);
+            createCheckoutButton(preference.id);
+
+        } catch (error) {
+            alert("error :c");
+        }
+    });
 
 
-        const orderData = {
-            title: "producto",
-            quantity: 1,
-            price: 10,
+    
+    const createCheckoutButton = (preferenceId) => {
+        const bricksBuilder = mp.bricks();
+
+        const renderComponent = async () => {
+            if (window.checkoutButton) window.checkoutButton.unmount();
+
+            await bricksBuilder.create("wallet", "wallet_container", {
+                initialization: {
+                    preferenceId: preferenceId,
+                },
+                customization: {
+                    texts: {
+                        valueProp: 'smart_option',
+                    },
+                },
+            });
         };
-    
-        const response  = await  fetch("http://localhost:8000/create_preference", {
-            method: "POST",
-            headers: {
-                "content-Type": "application/json",
-                
-            },
-            body: JSON.stringify(orderData),
-        });
-    
-        const preference = await response.JSON()
-    
-        createCheckoutButton(preference.id);
-
-    } catch(error){
-        alert("error")
-    } 
-
+        renderComponent();
+    };
 });
-
 
 
 const createCheckoutButton = (prederenceId) => {
--- core/static/core/server/index.js
+++ core/static/core/server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express());
 
 app.get("/", (req, res) => {
-    res.send("hola hola")
+    res.send("hola hojjla")
 });
 
 app.post("/create_preference", async (req, res) => {
